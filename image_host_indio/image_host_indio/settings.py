f4699495e6ae8190dadbdac170eed48adb4d0072
Eric Poels <epoels@ucsc.edu>
2018-10-02 17:34:16 -0700

    Change database setting to match PostgreSQL. Setup initial django database

--- image_host_indio/image_host_indio/settings.py
+++ image_host_indio/image_host_indio/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'image_host_indio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'indio_images',
+        'USER': 'ericpoels',
+        'PASSWORD': 'indio',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -116,5 +120,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+STATIC_ROOT = ''
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = ( os.path.join('static'), )
+
