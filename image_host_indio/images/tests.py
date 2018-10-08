6bd314d6b2c08ceac6c363f5a87db26f22f9f0d6
Eric Poels <epoels@ucsc.edu>
2018-10-08 16:30:54 -0700

    Created some tests

--- image_host_indio/images/tests.py
+++ image_host_indio/images/tests.py
@@ -1,14 +1,70 @@
 import datetime
+import tempfile
 
-from django.core.files.uploadedfile import SimpleUploadedFile
+from PIL import Image
+
+from django.test import override_settings
 from django.test import TestCase
 from django.utils import timezone
 
 from .models import User_Image
+from .forms import ImageForm
 
-# Create your tests here.
+def get_temporary_image(temp_file):
+    size = (200, 200)
+    color = (255, 0, 0, 0)
+    image = Image.new("RGB", size, color)
+    image.save(temp_file, 'jpeg')
+    return temp_file
 
-# Tests for model properties
+# Tests for creating model properties
 
-def test_image_width(self):
-	test = SimpleUploadedFile("file.jpg", "file_content", content_type="image/jpeg")
+class UserImageModelTests(TestCase):
+
+    @override_settings(MEDIA_ROOT=tempfile.gettempdir())
+    def test_create_image_test(self):
+        temp_file = tempfile.NamedTemporaryFile()
+        test_image = get_temporary_image(temp_file)
+        image = User_Image.objects.create(image=test_image.name)
+        self.assertEqual(len(User_Image.objects.all()), 1)
+
+    @override_settings(MEDIA_ROOT=tempfile.gettempdir())
+    def test_create_isPublic_true_test(self):
+        temp_file = tempfile.NamedTemporaryFile()
+        test_image = get_temporary_image(temp_file)
+        image = User_Image.objects.create(image=test_image.name, is_public=True)
+        test_query = User_Image.objects.first()
+        self.assertIs(test_query.is_public, True)
+
+    @override_settings(MEDIA_ROOT=tempfile.gettempdir())
+    def test_create_isPublic_false_test(self):
+        temp_file = tempfile.NamedTemporaryFile()
+        test_image = get_temporary_image(temp_file)
+        image = User_Image.objects.create(image=test_image.name, is_public=False)
+        test_query = User_Image.objects.first()
+        self.assertIs(test_query.is_public, False)
+
+    @override_settings(MEDIA_ROOT=tempfile.gettempdir())
+    def test_create_image_width_test(self):
+
+        """ The temporary image created has a width of 200, so we will
+        test if the image has the correct width dimension """
+
+        temp_file = tempfile.NamedTemporaryFile()
+        test_image = get_temporary_image(temp_file)
+        image = User_Image.objects.create(image=test_image.name,)
+        test_query = User_Image.objects.first()
+        self.assertEqual(test_query.image.width, 200)
+
+    @override_settings(MEDIA_ROOT=tempfile.gettempdir())
+    def test_create_image_height_test(self):
+
+    	""" The temporary image created has a height of 200, so we will
+    	test if the image has the correct height dimension """
+
+    	temp_file = tempfile.NamedTemporaryFile()
+    	test_image = get_temporary_image(temp_file)
+    	image = User_Image.objects.create(image=test_image.name,)
+    	test_query = User_Image.objects.first()
+    	self.assertEqual(test_query.image.height, 200)
+		
